16d50630533bdb818b8c3bfc8b1b9f1343591160
zshabbir-cc2526 <zullqarnainshabbir@gmail.com>
2025-11-14 16:53:20 +0100

    new code develop

--- verlet/verlet.f95
+++ verlet/verlet.f95
@@ -1,30 +1,53 @@
 PROGRAM verlet
   IMPLICIT NONE
-  INTEGER, PARAMETER :: wp = SELECTED_REAL_KIND (p=13, r=300)
-  INTEGER :: i, j, k                                                 
-  INTEGER :: l, m                                               
-  CHARACTER (LEN=72) :: str1, str2, str3       
-  REAL (KIND=wp) :: a, b, c                                          
-  REAL (KIND=wp), DIMENSION(:), ALLOCATABLE :: x
-  REAL (KIND=wp), DIMENSION(:,:), ALLOCATABLE :: xmat
-  
-  ALLOCATE ( x (300) )
-  ALLOCATE ( xmat (200,300) )
-  
-  a = b * c
-  
-  a = a + ( b * c)
-  
-  a = 3.0_wp
-  i = 1
-  j = 1
+  INTEGER, PARAMETER :: wp =SELECTED_REAL_KIND (p=13, r=300)
 
-  x(i) = 3.5_wp
-  
-  x(3) = 1.0E-12_wp
-  
-  xmat(i,j) = 9.2E6_wp
-  
-  PRINT *, x(3)
+  REAL (KIND=wp) :: tau
+  REAL (KIND=wp) :: m
+  REAL (KIND=wp) :: x, y, z, xn, yn, zn, v_x, v_y, v_z, v_xn, v_yn, v_zn, f_x, f_y, f_z, f_xn, f_yn, f_zn
+  INTEGER :: k, i
 
-END PROGRAM verlet
+  tau = 0.2_wp
+  m = 1_wp
+  x = 0_wp
+  y = 0_wp
+  z = 0_wp
+  v_x = 0_wp
+  v_y = 0_wp
+  v_z = 0_wp
+  f_x = 0_wp
+  f_y = 0.1_wp
+  f_z = 0_wp
+  f_xn = 0_wp
+  f_yn = 0.1_wp
+  f_zn = 0_wp
+  i = 100
+
+  DO k = 1, i
+    xn = x + tau * v_x + (tau * tau) * f_x / (2 * m)
+    yn = y + tau * v_y + (tau * tau) * f_y / (2 * m)
+    zn = z + tau * v_z + (tau * tau) * f_z / (2 * m)
+    v_xn = v_x + tau * (f_x + f_xn) / (2 * m)
+    v_yn = v_y + tau * (f_y + f_yn) / (2 * m)
+    v_zn = v_z + tau * (f_z + f_zn) / (2 * m)
+    v_x = v_xn
+    v_y = v_yn
+    v_z = v_zn
+    x = xn
+    y = yn
+    z = zn
+    f_x = f_xn
+    f_y = f_yn
+    f_z = f_zn
+
+    PRINT *, "The positions of the particle at the", k, "-th iteration at time =", tau * k, ":"
+    PRINT *, xn
+    PRINT *, yn
+    PRINT *, zn
+    PRINT *, v_xn
+    PRINT *, v_yn
+    PRINT *, v_zn
+
+  END DO
+
+END PROGRAM VERLET
